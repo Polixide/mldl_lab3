2903bdbb79c92a5d6e01ec544f6b659fe8ca5e18
DanieleC <dani.catalano02@gmail.com>
2025-03-18 14:01:22 +0100

    modified dataset downloader

--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -1,2 +1,7 @@
-!wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
-!unzip tiny-imagenet-200.zip -d data/tiny_imagenet
+import subprocess
+
+# Scarica il dataset
+subprocess.run(["wget", "http://cs231n.stanford.edu/tiny-imagenet-200.zip"])
+
+# Estrai i file
+subprocess.run(["unzip", "tiny-imagenet-200.zip", "-d", "data/tiny_imagenet"])
